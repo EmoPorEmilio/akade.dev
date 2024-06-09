e2c38a28d43948b2a3b3481e79857c48c4dee9bd
Emilio <franceschini.emilio@gmail.com>
2024-06-09 20:35:31 -0300

    setup vite

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+      <img src="/logo.png" class="logo vanilla" alt="Akade logo" />
+    </a>
+  </div>
+`
