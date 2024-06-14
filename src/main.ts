94a9c7b08faa245ddcbdf4f0efebcae0dbd3cd50
Emilio <franceschini.emilio@gmail.com>
2024-06-13 21:04:00 -0300

    draw ring on canvas

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,25 @@ import './style.css'
 document.querySelector<HTMLDivElement>('body')!.innerHTML = `
 <main>
   <div class="logo-container">
-      <img class="logo" src="/logo.png" />
+      <canvas id="main-canvas"></canvas>
+      <img class="logo" src="/logo.png"></img>
   </div>
 </main>
-`
+`;
+
+const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+canvas.width = document.body.getBoundingClientRect().width;
+canvas.height = document.body.getBoundingClientRect().height;
+const context = canvas.getContext('2d')!;
+const colors = {
+  fuchsia: '#DF5C9A',
+  background: '#141414'
+}
+context.fillStyle = colors.fuchsia;
+context.beginPath();
+context.arc(92, canvas.height / 2, 55, 0, 2 * Math.PI);
+context.fill();
+context.beginPath();
+context.fillStyle = colors.background;
+context.arc(92, canvas.height / 2, 40, 0, 2 * Math.PI);
+context.fill();
